1e7c4eacb5dd0dc19d7bbba90ec76586bbc9f700
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 19:38:58 -0300

    Update Pollito.java

--- Herencia_Tipo/src/ejercicio_1/Pollito.java
+++ Herencia_Tipo/src/ejercicio_1/Pollito.java
@@ -1,5 +1,13 @@
 package ejercicio_1;
 
-public class Pollito {
+public class Pollito extends Animal implements Emisor_De_Sonido{
+
+	Pollito(String nombre, String sonido) {
+		super(nombre, sonido);
+	}
+	
+	public void emiteSonido() {
+		System.out.println("Quak");
+	}
 
 }
