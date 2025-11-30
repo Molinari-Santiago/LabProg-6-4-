842168105341ed9f5d60551bb915b310399f63f8
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 19:39:20 -0300

    Update Vaca.java

--- Herencia_Tipo/src/ejercicio_1/Vaca.java
+++ Herencia_Tipo/src/ejercicio_1/Vaca.java
@@ -1,5 +1,12 @@
 package ejercicio_1;
 
-public class Vaca {
+public class Vaca extends Animal implements Emisor_De_Sonido{
 
+	Vaca(String nombre, String sonido) {
+		super(nombre, sonido);
+	}
+	
+	public void emiteSonido() {
+		System.out.println("Muu");
+	}
 }
