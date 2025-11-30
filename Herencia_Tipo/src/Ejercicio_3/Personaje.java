84e8bb4f8ecdd1e5dd0a6d8dda3ff6b642d51884
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 19:46:52 -0300

    Update Personaje.java

--- Herencia_Tipo/src/Ejercicio_3/Personaje.java
+++ Herencia_Tipo/src/Ejercicio_3/Personaje.java
@@ -1,5 +1,27 @@
 package Ejercicio_3;
 
-public class Personaje {
+public class Personaje implements Dibujable{
+	private String nombre;
+	private int vida;
+	private Posicion posicion;
+	
+	public Personaje(String nombre, int vida, Posicion posicion) {
+		this.nombre = nombre;
+		this.vida = vida;
+		this.posicion = posicion;
+	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public int getVida() {
+		return vida;
+	}
+
+	@Override
+	public void RepresentacionEnConsola() {
+		System.out.println("nombre: " + this.nombre + " y posicion en x: " + this.posicion.getPosx() + " y posicion en y: " + this.posicion.getPosy());
+		
+	}
 }
