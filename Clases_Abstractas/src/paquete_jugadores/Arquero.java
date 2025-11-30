41856b555a30bd3ea2f23e535a480e566be07284
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 16:21:25 -0300

    Update Arquero.java

--- Clases_Abstractas/src/paquete_jugadores/Arquero.java
+++ Clases_Abstractas/src/paquete_jugadores/Arquero.java
@@ -1,5 +1,25 @@
 package paquete_jugadores;
 
-public class Arquero {
+public class Arquero extends Jugador{
+	private double efectividad;
+	public Arquero(String nombre, double velocidad, double potencia) {
+		super(nombre, velocidad, potencia);
+	}
 
+	@Override
+	public double indiceDefensa() {
+		return this.efectividad;
+	}
+
+	@Override
+	public double indiceAtaque() {
+		return (0.1 * this.velocidad * this.potencia);
+	}
+
+	@Override
+	public String toString() {
+		return "Arquero [efectividad=" + efectividad + ", indiceDefensa()=" + indiceDefensa() + ", indiceAtaque()="
+				+ indiceAtaque() + "]";
+	}
+	
 }
