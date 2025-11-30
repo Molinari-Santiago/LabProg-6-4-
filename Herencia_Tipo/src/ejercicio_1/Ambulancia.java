83bb2c278159b2aca43df24e0d64190dc5977f49
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 19:36:02 -0300

    Update Ambulancia.java

--- Herencia_Tipo/src/ejercicio_1/Ambulancia.java
+++ Herencia_Tipo/src/ejercicio_1/Ambulancia.java
@@ -1,5 +1,13 @@
 package ejercicio_1;
 
-public class Ambulancia {
+public class Ambulancia extends Vehiculo implements Emisor_de_Sonido{
+
+	public Ambulancia(String Patente, String numeroChasis, String numeroMotor) {
+		super(Patente, numeroChasis, numeroMotor);
+	}
+
+	public void emiteSonido() {
+		System.out.println("sirena sonando");
+	}
 
 }
