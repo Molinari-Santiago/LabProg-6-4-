b5933e1f989c42cbd16326f41e5870d8de1a69c7
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 19:33:20 -0300

    Update Curso.java
    Added properties and methods to manage course details.

--- Interfaces_Graficas_3ra_Parte/src/paquete/Curso.java
+++ Interfaces_Graficas_3ra_Parte/src/paquete/Curso.java
@@ -1,5 +1,27 @@
 package paquete;
+import java.util.ArrayList;
 
 public class Curso {
+    private String nombre; 
+    private ArrayList<Asignaturas> asignaturas = new ArrayList<Asignaturas>();
+     
+     public Curso(String nombre, ArrayList<Asignaturas> asignaturas) {
+         this.nombre = nombre;
+         this.asignaturas = asignaturas;
+     }
 
-}
+    public String getNombre() {
+        return nombre;
+    }
+
+    public ArrayList<Asignaturas> getAsignaturas() {
+        return asignaturas;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " - Asignaturas: " + asignaturas.size();
+    }
+	}
+
+
