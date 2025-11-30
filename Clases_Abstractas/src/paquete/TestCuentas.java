0af2f791ed9f5142d211a9124bf755c3d468044e
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 16:20:11 -0300

    Update TestCuentas.java

--- Clases_Abstractas/src/paquete/TestCuentas.java
+++ Clases_Abstractas/src/paquete/TestCuentas.java
@@ -2,4 +2,34 @@ package paquete;
 
 public class TestCuentas {
 
+public static void main(String[] args) {
+		CuentaBancaria c1 = new CuentaUniversitaria("Mateo");
+		c1.deposito(700);
+		System.out.println(c1);
+		c1.extraer(300);
+		System.out.println(c1);
+		c1.extraer(300);
+		System.out.println(c1);
+		c1.extraer(700);
+		System.out.println(c1);
+		
+		
+		CuentaBancaria c2 = new CuentaCorriente("Lautaro");
+		System.out.println(c2);
+		c2.extraer(300);
+		System.out.println(c2);
+		c2.extraer(300);
+		System.out.println(c2);
+		c2.extraer(700);
+		System.out.println(c2);
+		
+		CuentaBancaria c3 = new CajaDeAhorro("Elisa");
+		System.out.println(c3);
+		c3.extraer(300);
+		System.out.println(c3);
+		c3.extraer(300);
+		System.out.println(c3);
+		c3.extraer(700);
+		System.out.println(c3);
+	}
 }
