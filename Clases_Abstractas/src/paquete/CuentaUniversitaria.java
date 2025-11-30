985116bda6568fd02d1d29a48234ab3c0d9f86b4
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 16:19:41 -0300

    Update CuentaUniversitaria.java

--- Clases_Abstractas/src/paquete/CuentaUniversitaria.java
+++ Clases_Abstractas/src/paquete/CuentaUniversitaria.java
@@ -1,5 +1,23 @@
 package paquete;
 
-public class CuentaUniversitaria {
+public class CuentaUniversitaria extends CuentaBancaria{
+	private int acumuladorDiario = 0;
+	public CuentaUniversitaria(String titular) {
+		super(titular);
+	}
 
+	@Override
+	public boolean extraer(double cantidad) {
+		boolean respuesta = false;
+		if (this.acumuladorDiario + cantidad <= 1000 && cantidad <= this.saldo) {
+			this.saldo -= cantidad;
+			this.acumuladorDiario += cantidad;
+			respuesta = true;
+		}
+		return respuesta;
+	}
+	public void resetAcumuladorDiario() {
+		this.acumuladorDiario = 0;
+	}
 }
+
