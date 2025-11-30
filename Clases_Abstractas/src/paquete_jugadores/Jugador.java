4dfcbab1ca68855e1fdd59fbc93dcad6c26a4a24
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 16:23:52 -0300

    Update Jugador.java

--- Clases_Abstractas/src/paquete_jugadores/Jugador.java
+++ Clases_Abstractas/src/paquete_jugadores/Jugador.java
@@ -1,5 +1,29 @@
 package paquete_jugadores;
 
-public class Jugador {
+public abstract class Jugador {
+	protected String nombre;
+	protected double velocidad;
+	protected double potencia;
+	
+	public Jugador(String nombre, double velocidad, double potencia) {
+		this.nombre = nombre;
+		this.velocidad = velocidad;
+		this.potencia = potencia;
+	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public double getVelocidad() {
+		return velocidad;
+	}
+
+	public double getPotencia() {
+		return potencia;
+	}
+	
+	public abstract double indiceDefensa();
+	public abstract double indiceAtaque();
+	
 }
