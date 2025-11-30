3c6258f2f10902018e3a785968785d29f3c98e26
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 16:18:53 -0300

    Update CuentaCorriente.java

--- Clases_Abstractas/src/paquete/CuentaCorriente.java
+++ Clases_Abstractas/src/paquete/CuentaCorriente.java
@@ -1,5 +1,28 @@
 package paquete;
 
-public class CuentaCorriente {
-
+ublic class CuentaCorriente extends CuentaBancaria {
+	private double giroEnDescubierto;
+	public CuentaCorriente(String titular) {
+		super(titular);
+	}
+	public double getGiroEnDescubierto() {
+		return this.giroEnDescubierto;
+	}
+	public void setGiroEnDescubierto(double giroEnDescubierto) {
+		this.giroEnDescubierto = giroEnDescubierto;
+	}
+	@Override
+	public boolean extraer(double cantidad) {
+		boolean respuesta = false;
+		if (this.saldo >= cantidad) {
+			this.saldo -= cantidad;
+			respuesta = true;
+		} else if ((this.saldo + this.giroEnDescubierto) >= cantidad) {
+			this.saldo -= cantidad;
+			this.giroEnDescubierto += this.saldo;
+			respuesta = true;
+		}
+		return respuesta;
+	}
+	
 }
