96481bd4fcd479596f137b028d8b4590a504659f
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 19:41:17 -0300

    Update Main.java
    Added main method to initialize characters and demonstrate position changes.

--- Herencia_Tipo/src/ejercicio_2/Main.java
+++ Herencia_Tipo/src/ejercicio_2/Main.java
@@ -1,5 +1,29 @@
 package ejercicio_2;
 
-public class Main {
+import java.util.ArrayList;
 
+public class Main {
+    public static void main(String[] args) {
+    	ArrayList<Personaje> personajes = new ArrayList<>();
+        
+    	Posicion p1 = new Posicion(3, 3);
+    	Posicion p2 = new Posicion(4, 8);
+    	Posicion p3 = new Posicion(0, 9);
+    	
+    	personajes.add(new Caballero("Yo", 10, p1));
+    	personajes.add(new Mago("Magico", 10, p2));
+    	personajes.add(new Ogro("Feo", 10, p3));
+        
+		System.out.println(p1.getPosx() + " y " + p1.getPosy() + "antes");
+    	p1.incrementaPos(1, 5);
+    	System.out.println(p1.getPosx() + " y " + p1.getPosy() + "despues");
+    	
+    	System.out.println(p2.getPosx() + " y " + p2.getPosy()  + "antes");
+    	p2.setXY(10, 80);
+    	System.out.println(p2.getPosx() + " y " + p2.getPosy() + "despues");
+    	
+    	System.out.println(p3.getPosx() + " y " + p3.getPosy()  + "antes");
+    	p3.incrementaPos(3, 0);
+    	System.out.println(p3.getPosx() + " y " + p3.getPosy() + "despues");
+    }
 }
