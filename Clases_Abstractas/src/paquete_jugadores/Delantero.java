0b31c6093fc1896a7ca4b7c37daa6cc229a4244f
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 16:22:13 -0300

    Update Delantero.java

--- Clases_Abstractas/src/paquete_jugadores/Delantero.java
+++ Clases_Abstractas/src/paquete_jugadores/Delantero.java
@@ -1,5 +1,22 @@
 package paquete_jugadores;
 
-public class Delantero {
+public class Delantero extends Jugador{
+	public Delantero(String nombre, double velocidad, double potencia) {
+		super(nombre, velocidad, potencia);
+	}
 
+	@Override
+	public double indiceDefensa() {
+		return (this.velocidad * 0.5);
+	}
+
+	@Override
+	public double indiceAtaque() {
+		return (this.velocidad * this.potencia);
+	}
+
+	@Override
+	public String toString() {
+		return "Delantero [indiceDefensa()=" + indiceDefensa() + ", indiceAtaque()=" + indiceAtaque() + "]";
+	}
 }
