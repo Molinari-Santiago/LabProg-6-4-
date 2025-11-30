363b526a6473de98d340303929ce99fa571ba49a
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 16:33:03 -0300

    Update TestCuentaBancaria.java

--- Constructores/src/paquete/TestCuentaBancaria.java
+++ Constructores/src/paquete/TestCuentaBancaria.java
@@ -1,5 +1,10 @@
 package paquete;
 
-public class TestCuentaBancaria {
-
+public class prueba {
+	 public static void main(String[] args) {
+		 CuentaBancaria c = new CuentaBancaria("mateo", 9999999, 3, 15);
+		 c.depositar(99);
+		 c.extraer(1);
+		 System.out.println(c.toString());
+	 }
 }
