d5cee366b1f02981c98793593d9cc88c79a86557
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 19:42:32 -0300

    Update Personaje.java

--- Herencia_Tipo/src/ejercicio_2/Personaje.java
+++ Herencia_Tipo/src/ejercicio_2/Personaje.java
@@ -1,5 +1,21 @@
 package ejercicio_2;
 
 public class Personaje {
+	private String nombre;
+	private int vida;
+	private Posicion posicion;
+	
+	public Personaje(String nombre, int vida, Posicion posicion) {
+		this.nombre = nombre;
+		this.vida = vida;
+		this.posicion = posicion;
+	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public int getVida() {
+		return vida;
+	}
 }
