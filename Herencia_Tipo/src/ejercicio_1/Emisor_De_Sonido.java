a8bb01c787d628d2c6076a9770cd5c01dafe5bb6
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 19:37:36 -0300

    Update Emisor_De_Sonido.java

--- Herencia_Tipo/src/ejercicio_1/Emisor_De_Sonido.java
+++ Herencia_Tipo/src/ejercicio_1/Emisor_De_Sonido.java
@@ -1,12 +1,5 @@
 package ejercicio_1;
 
-public class Gato extends Animal implements Emisor_De_Sonido{
-
-	Gato(String nombre, String sonido) {
-		super(nombre, sonido);
-	}
-	
-	public void emiteSonido() {
-		System.out.println("Miau");
-	}
+public interface Emisor_De_Sonido {
+	void emiteSonido();
 }
