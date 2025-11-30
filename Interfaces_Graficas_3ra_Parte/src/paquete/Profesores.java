a7517491f24c1a1c7b31cce605391868ce1c89d4
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 19:34:46 -0300

    Update Profesores.java

--- Interfaces_Graficas_3ra_Parte/src/paquete/Profesores.java
+++ Interfaces_Graficas_3ra_Parte/src/paquete/Profesores.java
@@ -1,5 +1,27 @@
 package paquete;
 
-public class Profesores {
+import java.util.ArrayList;
 
+public class Profesores extends Persona{
+    private String CUIL;
+    private ArrayList<Asignaturas> asignaturasDadas = new ArrayList<Asignaturas>();
+
+    public Profesores(String nombre, String dni, String CUIL, ArrayList<Asignaturas> asignaturasDadas) {
+        super(nombre, dni);
+        this.CUIL = CUIL;
+        this.asignaturasDadas = asignaturasDadas;
+    }
+
+    public String getCUIL() {
+        return CUIL;
+    }
+    
+    public ArrayList<Asignaturas> getAsignaturasDadas() {
+        return asignaturasDadas;
+    }
+
+    @Override
+    public String toString() {
+        return "Profesor: " + getNombre() + " - CUIL: " + CUIL + " - DNI: " + getDni();
+    }
 }
