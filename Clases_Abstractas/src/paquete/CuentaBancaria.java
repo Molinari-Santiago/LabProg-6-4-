f64c2f0b1fb7bfea7fa4743fe64d4476fe310235
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 16:17:16 -0300

    Update CuentaBancaria.java

--- Clases_Abstractas/src/paquete/CuentaBancaria.java
+++ Clases_Abstractas/src/paquete/CuentaBancaria.java
@@ -1,5 +1,19 @@
 package paquete;
 
-public class CuentaBancaria {
-
+public abstract class CuentaBancaria {
+	protected String titular;
+	protected double saldo;
+	public CuentaBancaria(String titular) {
+		this.titular = titular;
+	}
+	public String getTitular() {
+		return this.titular;	
+	}
+	public double getSaldo() {
+		return this.saldo;
+	}
+	public void deposito(double cantidad) {
+		this.saldo += cantidad;
+	}
+	public abstract boolean extraer(double cantidad);
 }
