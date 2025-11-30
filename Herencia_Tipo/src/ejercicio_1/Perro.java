59018e0818f7b1edef5712a786f1e7b65043828e
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 19:38:38 -0300

    Update Perro.java

--- Herencia_Tipo/src/ejercicio_1/Perro.java
+++ Herencia_Tipo/src/ejercicio_1/Perro.java
@@ -1,5 +1,12 @@
 package ejercicio_1;
 
-public class Perro {
+public class Perro extends Animal implements Emisor_De_Sonido{
 
+	Perro(String nombre, String sonido) {
+		super(nombre, sonido);
+	}
+	
+	public void emiteSonido() {
+		System.out.println("Guau");
+	}
 }
