fc24c9d9d17bb0c59d365692cec23d01328ce96e
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 19:38:17 -0300

    Update Main.java

--- Herencia_Tipo/src/ejercicio_1/Main.java
+++ Herencia_Tipo/src/ejercicio_1/Main.java
@@ -1,5 +1,29 @@
 package ejercicio_1;
 
-public class Main {
+import java.util.ArrayList;
 
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Animal> animales = new ArrayList<>();
+        ArrayList<Vehiculo> vehiculos = new ArrayList<>();
+        
+        animales.add(new Perro("Perro", "Guau"));
+        animales.add(new Gato("Gato", "Miau"));
+        animales.add(new Vaca("Vaca", "Muu"));
+        animales.add(new Pollito("Pollito", "Quuak"));
+        
+        vehiculos.add(new Ambulancia("123", "1", "4"));
+        
+        for (Animal a : animales) {
+            if (a instanceof Emisor_De_Sonido) {
+                ((Emisor_De_Sonido) a).emiteSonido(); 
+            }
+        }
+        
+        for (Vehiculo v : vehiculos) {
+            if (v instanceof Emisor_De_Sonido) {
+                ((Emisor_De_Sonido) v).emiteSonido(); 
+            }
+        }
+    }
 }
