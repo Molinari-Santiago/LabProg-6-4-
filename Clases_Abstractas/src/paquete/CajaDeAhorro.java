d1d064480c77a416fbe9871823c08735fe81fec9
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 16:16:42 -0300

    Update CajaDeAhorro.java

--- Clases_Abstractas/src/paquete/CajaDeAhorro.java
+++ Clases_Abstractas/src/paquete/CajaDeAhorro.java
@@ -1,5 +1,18 @@
 package paquete;
 
-public class CajaDeAhorro {
-
+public class CajaDeAhorro extends CuentaBancaria{
+	private int cantidadExtracciones;
+	public CajaDeAhorro(String titular) {
+		super(titular);
+	}
+	@Override
+	public boolean extraer(double cantidad) {
+		boolean respuesta = false;
+		if (this.cantidadExtracciones > 0 && this.saldo >= cantidad) {
+			this.cantidadExtracciones--;
+			this.saldo -= cantidad;
+			respuesta = true;
+		}
+		return respuesta;
+	}
 }
