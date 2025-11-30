a3cdddd9b50fb72cb04ad6880036f354e231e318
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 16:26:56 -0300

    Update Prueba.java
    Added main method to test player addition and display.

--- Clases_Abstractas/src/paquete_jugadores/Prueba.java
+++ Clases_Abstractas/src/paquete_jugadores/Prueba.java
@@ -1,5 +1,18 @@
 package paquete_jugadores;
 
-public class Prueba {
+import java.util.ArrayList;
 
+public class Prueba {
+    public static void main(String[] args) {
+        ArrayList<Jugador> listaJugadores = new ArrayList<>();
+
+        Equipo e = new Equipo("Club A", listaJugadores);
+
+        System.out.println(e.agregarJugador(new Delantero("j1", 1.0, 0.7))); 
+        System.out.println(e.agregarJugador(new Defensor("j2", 0.8, 0.5))); 
+        System.out.println(e.agregarJugador(new Arquero("j3", 0.1, 0.4))); 
+        System.out.println(e.agregarJugador(new Delantero("j4", 0.9, 0.6)));  
+
+        e.mostrarNombresJugadores();
+    }
 }
