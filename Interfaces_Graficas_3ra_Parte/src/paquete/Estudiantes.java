0c3b8eab574701a849a79ce13fd1c145ac94dbbe
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 19:33:43 -0300

    Update Estudiantes.java

--- Interfaces_Graficas_3ra_Parte/src/paquete/Estudiantes.java
+++ Interfaces_Graficas_3ra_Parte/src/paquete/Estudiantes.java
@@ -1,5 +1,19 @@
 package paquete;
 
-public class Estudiantes {
+public class Estudiantes extends Persona{
+    private String NLegajo;
+    
+    public Estudiantes(String nombre, String dni, String NLegajo) {
+        super(nombre, dni);
+        this.NLegajo = NLegajo;
+    }
 
+    public String getNLegajo() {
+        return NLegajo;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante: " + getNombre() + " - Legajo: " + NLegajo + " - DNI: " + getDni();
+    }    
 }
