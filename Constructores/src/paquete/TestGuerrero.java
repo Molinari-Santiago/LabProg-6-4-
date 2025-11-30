a37c70a47936aafca221316b212b3e2dccd4e7a7
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 16:34:43 -0300

    Update TestGuerrero.java

--- Constructores/src/paquete/TestGuerrero.java
+++ Constructores/src/paquete/TestGuerrero.java
@@ -1,5 +1,15 @@
 package paquete;
 
 public class TestGuerrero {
-
+public static void main(String[] args) {
+		Guerrero g1 = new Guerrero("hercules");
+		g1.mover(8, 10);
+		g1.recibeDano(100);
+		System.out.println(g1.toString());
+		
+		Guerrero g2 = new Guerrero("atena", 55, 6);
+		g2.mover(10, 5);
+		g2.recibeDano(2);
+		System.out.println(g2.toString());
+	}
 }
