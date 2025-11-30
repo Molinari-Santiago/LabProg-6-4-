4016ffc7f42a7a57e4a3c3d0fa558f4ed364df09
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 19:32:23 -0300

    Update Asignaturas.java

--- Interfaces_Graficas_3ra_Parte/src/paquete/Asignaturas.java
+++ Interfaces_Graficas_3ra_Parte/src/paquete/Asignaturas.java
@@ -1,5 +1,24 @@
 package paquete;
 
 public class Asignaturas {
+	 private String nombre;
+	    
+	    public Asignaturas(String nombre) {
+	        this.nombre = nombre;
+	    }
 
+	    public String getNombre() {
+	        return nombre;
+	    }
+
+	    public void setNombre(String nombre) {
+	        this.nombre = nombre;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return nombre;
+	    }
+	
 }
+
