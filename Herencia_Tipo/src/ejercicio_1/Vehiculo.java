f6fee317ebe6e167676847a72171ab3877ec5c92
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 19:39:39 -0300

    Update Vehiculo.java
    Added properties and methods for vehicle attributes.

--- Herencia_Tipo/src/ejercicio_1/Vehiculo.java
+++ Herencia_Tipo/src/ejercicio_1/Vehiculo.java
@@ -1,5 +1,37 @@
 package ejercicio_1;
 
-public class Vehiculo {
+public class Vehiculo{
+	private String Patente;
+	private String numeroChasis;
+	private String numeroMotor;
+	
+	public Vehiculo(String Patente, String numeroChasis, String numeroMotor) {
+		this.Patente = Patente;
+		this.numeroChasis = numeroChasis;
+		this.numeroMotor = numeroMotor;
+	}
 
+	public String getPatente() {
+		return Patente;
+	}
+
+	public void setPatente(String patente) {
+		Patente = patente;
+	}
+
+	public String getNumeroChasis() {
+		return numeroChasis;
+	}
+
+	public void setNumeroChasis(String numeroChasis) {
+		this.numeroChasis = numeroChasis;
+	}
+
+	public String getNumeroMotor() {
+		return numeroMotor;
+	}
+
+	public void setNumeroMotor(String numeroMotor) {
+		this.numeroMotor = numeroMotor;
+	}	
 }
