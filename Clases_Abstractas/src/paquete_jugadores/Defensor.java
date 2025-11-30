f004d58dea3b6560e05a5dc481ab98e26f638df3
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 16:21:51 -0300

    Update Defensor.java

--- Clases_Abstractas/src/paquete_jugadores/Defensor.java
+++ Clases_Abstractas/src/paquete_jugadores/Defensor.java
@@ -1,5 +1,23 @@
 package paquete_jugadores;
 
-public class Defensor {
+public class Defensor extends Jugador{
+	public Defensor(String nombre, double velocidad, double potencia) {
+		super(nombre, velocidad, potencia);
+	}
 
+	@Override
+	public double indiceDefensa() {
+		return (this.velocidad * this.velocidad);
+	}
+
+	@Override
+	public double indiceAtaque() {
+
+		return (this.potencia * this.potencia);
+	}
+
+	@Override
+	public String toString() {
+		return "Defensor [indiceDefensa()=" + indiceDefensa() + ", indiceAtaque()=" + indiceAtaque() + "]";
+	}
 }
