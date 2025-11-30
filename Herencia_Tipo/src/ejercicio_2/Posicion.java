681938dcda0c1ee6ab94081d1ed04d32ab5ccbcf
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2025-11-30 19:42:53 -0300

    Update Posicion.java

--- Herencia_Tipo/src/ejercicio_2/Posicion.java
+++ Herencia_Tipo/src/ejercicio_2/Posicion.java
@@ -1,5 +1,33 @@
 package ejercicio_2;
 
-public class Posicion {
+public class Posicion implements Movible{
+	private int x;
+	private int y;
+	
+	public Posicion(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 
+	@Override
+	public int getPosx() {
+		return this.x;
+	}
+
+	@Override
+	public int getPosy() {
+		return this.y;
+	}
+
+	@Override
+	public void setXY(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public void incrementaPos(int valorX, int valorY) {
+		this.x += x;
+		this.y += y;
+	}
 }
